7551f4ea1597980d02b403ddb4890d4f979476a5
ronakbhalara <131962409+ronakbhalara@users.noreply.github.com>
2025-01-08 21:21:22 +0530

    ##

--- next.config.js
+++ next.config.js
@@ -2,34 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    images: {
-        domains: ['example.com'], 
-        unoptimized: true, 
-    },
-    env: {
-        NEXT_PUBLIC_API_URL: 'https://api.example.com', 
-    },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'X-XSS-Protection',
-                        value: '1; mode=block',
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: "480px",
+        ssm: "600px",
+        md: "790px",
+        xl: "880px",
+        "1xl": "1140px",
+        "2xl": "1280px",
+      },
+    },
   },
   plugins: [],
 };
