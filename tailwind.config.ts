d667f3f7a9b7760d4780e54b87b8874cd4b76049
ronakbhalara <131962409+ronakbhalara@users.noreply.github.com>
2025-01-08 21:10:41 +0530

    ##

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,17 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    container: {
-      center: true,
-      screens: {
-        sm: "480px",
-        ssm: "600px",
-        md: "790px",
-        xl: "880px",
-        "1xl": "1140px",
-        "2xl": "1280px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
